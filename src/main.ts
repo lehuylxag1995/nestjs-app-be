350b4fb6673e63082b56373c7cb16a4bfaaf9b67
Lê Huy <lehuylxag1995@gmail.com>
2025-09-21 11:13:30 +0700

    Cập nhật tính nẳng bảo mật cơ bản gói helmet, bật CORS cơ bản

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //Cài đặt CORS (cho phép/từ chối request từ domain bên ngoài vào app)
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Cho phép cookie/authorization header
+  });
 
-  //Setup validation auto
+  //Cài đặt bảo mật cho HTTP cơ bản
+  app.use(helmet());
+
+  //Cài đặt Pipes toàn cục
   app.useGlobalPipes(
     new ValidationPipe({
-      stopAtFirstError: true,
+      stopAtFirstError: true, // Dừng tại lỗi đầu tiên
       whitelist: true, // Bỏ qua các DTO không validation
       transform: true, // bật chức năng auto. Nhưng cần khai báo rõ @Type() trong DTO
     }),
